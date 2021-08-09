63edb51f73d34f28ed6b8cb21e1b6afa503e5892
Kendall Arneaud <kendall.arneaud@gmail.com>
2021-08-09 19:35:35 -0400

    Update method
    use TransactionModificationResponse

--- src/Message/CancelRecurringRequest.php
+++ src/Message/CancelRecurringRequest.php
@@ -18,4 +18,16 @@ class CancelRecurringRequest extends AbstractTransactionModificationRequest
      * @var int;
      */
     protected $modificationType = 4;
+    
+    /**
+     * Return the transaction modification response object
+     *
+     * @param \SimpleXMLElement $xml Response xml object
+     *
+     * @return ResponseInterface
+     */
+    protected function newResponse($xml)
+    {
+        return new Response($this, $xml);
+    }
 }
